e0f21c9127b2679258cfeba08498043194e2349a
Yea Saleh <yeasalehofficial2002@gmail.com>
2021-10-18 17:47:16 +0600

    date

--- src/components/AllFeatures/Doctor/Doctor.js
+++ src/components/AllFeatures/Doctor/Doctor.js
@@ -1,10 +1,156 @@
 import React from 'react';
 import './Doctor.css';
 
+
+
+
 const Doctor = () => {
     return (
-        <div>
-            <h1>Doctor</h1>
+        <div className="container">
+              <div className="row">
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/6PG52CM/child-1.jpg" alt="" />
+                      </div>
+                      <div>
+                          <h5 className="bg-dark text-white">Fayez Karim</h5>
+                          <h5  className="bg-dark text-white">Child Specialist</h5>
+                      </div>
+                  </div>
+                  
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/M50HxQv/child-2.jpg" alt="" />
+                      </div>
+
+                      <div>
+                          <h5 className="bg-dark text-white">Shadath Hossain</h5>
+                          <h5  className="bg-dark text-white">Child Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/LpF0xBM/child-3.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Nazmul Houqe</h5>
+                          <h5  className="bg-dark text-white">Child Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/YZmcxb2/dental-1.jpg" alt="" />
+                      </div>
+                     
+                      <div>
+                          <h5 className="bg-dark text-white">Siam Ahmed</h5>
+                          <h5  className="bg-dark text-white">Dental Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/hVqZtRB/dental-2.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Foysal Mahmud</h5>
+                          <h5  className="bg-dark text-white">Dental Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/F4sZXt8/Portrait-of-handsome-eye-doctor-sitting-with-ophthalmologic-device-in-the-cabinet.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Motin Khan</h5>
+                          <h5  className="bg-dark text-white">Eye Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/rsv4Zwj/eye-2.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Anamul Houqe</h5>
+                          <h5  className="bg-dark text-white">Eye Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/rmHCr1t/medicin-1-jpg.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Junayed Yeafin</h5>
+                          <h5  className="bg-dark text-white">Medicine Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                     <div>
+                     <img src="https://i.ibb.co/B3zj83R/medicin-2-jpg.jpg" alt="" />
+                     </div>
+                     
+                     <div>
+                          <h5 className="bg-dark text-white">Naime Molla</h5>
+                          <h5  className="bg-dark text-white">Medicine Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/TvD1C6T/medicin-3-jpg.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Rayhan ahmed</h5>
+                          <h5  className="bg-dark text-white">Medicine Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                      <div>
+                      <img src="https://i.ibb.co/DGxvBcy/urology-1.jpg" alt="" />
+                      </div>
+                      
+                      <div>
+                          <h5 className="bg-dark text-white">Mahmudul Hasan</h5>
+                          <h5  className="bg-dark text-white">Urology Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                     <div>
+                     <img src="https://i.ibb.co/SxM4nhg/urology-2.jpg" alt="" />
+                     </div>
+                     
+                     <div>
+                          <h5 className="bg-dark text-white">Arefin Hossain</h5>
+                          <h5  className="bg-dark text-white">Urology Specialist</h5>
+                      </div>
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-4">
+                     <div>
+                     <img src="https://i.ibb.co/J56kYQ8/urology-3.jpg" alt="" />
+                     </div>
+                     
+                     <div>
+                          <h5 className="bg-dark text-white">Yasin Ali</h5>
+                          <h5  className="bg-dark text-white">Urology Specialist</h5>
+                      </div>
+                  </div>
+
+              </div>
         </div>
     );
 };
